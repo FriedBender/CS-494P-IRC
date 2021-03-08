91d502824a5e9e61b10273df20d03a84618e5280
Brandon <brandonaltermatt@gmail.com>
2021-03-08 02:10:51 -0800

    Refactored chatrooms, removed members. Added message parser and commands.

--- Client.py
+++ Client.py
@@ -4,14 +4,13 @@ IRC - Client Application
 """
 
 
-import socket, select, sys
+import socket, select, sys, Server
+from Server import BUFFER_SIZE
 
-BUFFER_SIZE = 2048  # Define the maxiumum message buffer size
-
-"""# Give the user a prompt for input
+# Give the user a prompt for input
 def user_input(username):
     sys.stdout.write(f"{username} > ")
-    sys.stdout.flush()"""
+    sys.stdout.flush()
 
 
 def irc_client():
@@ -19,7 +18,7 @@ def irc_client():
     host = socket.gethostname()
     port = 5050
 
-    #username = input("Username: ")
+    username = input("Enter username: ")
 
     # Create the server socket and connect to the server
     server_socket = socket.socket()
@@ -27,7 +26,7 @@ def irc_client():
     print(f"Connected to server at {host}:{port}")
 
     # Send initial message to server with username
-    #server_socket.send(username.encode())
+    server_socket.send(username.encode())
 
     # Loop to receive and send messages
     while True:
@@ -40,7 +39,7 @@ def irc_client():
 
             # Handle message from server
             if notified_socket == server_socket:
-                message = server_socket.recv(BUFFER_SIZE).decode()
+                message = server_socket.recv(Server.BUFFER_SIZE).decode()
                 # If server shuts down, recv will return an empty string
                 if not message:
                     server_socket.shutdown(2)
@@ -51,13 +50,13 @@ def irc_client():
                 sys.stdout.write('\r')
                 sys.stdout.flush()
                 sys.stdout.write(message)
-                #user_input(username)
+                user_input(username)
                 
             # Handle input from user
             else:
                 message = sys.stdin.readline()
                 server_socket.send(message.encode())
-                #user_input(username)
+                user_input(username)
 
     server_socket.close()   #close connection
 
--- Server.py
+++ Server.py
@@ -3,79 +3,72 @@ CS 494P Project
 IRC - Server Application
 """
 
-import socket, select, sys
-from ChatRooms import IRC_Application, ChatRoom, Member
-import ChatRooms
+import socket, select, Chatrooms
+from Chatrooms import IRC_Application, Chatroom
 
+# Globals
+PORT_NUMBER = 5050
 SOCKET_LIST = []   # Maintain a list of socket connections
 CLIENTS = {}    # Maintain a dictionary of clients. The key is the socket, the value is the username
-irc_Application = IRC_Application()  # The object to handle the IRC side of things.
-new_room = ChatRoom(ChatRooms.DEFAULT_ROOM_NAME)    # creates a new room
-irc_Application.rooms[ChatRooms.DEFAULT_ROOM_NAME] = new_room  # puts the new default room into the rooms
+BUFFER_SIZE = 2048  # Define the maxiumum message buffer size
+MAX_NUMBER_OF_CLIENTS = 10  # Maxmimum number of clients
 
-"""
-# Broadcast a message to all clients
-def message_broadcast(sender_socket, message):
-    user = CLIENTS[sender_socket]
-    print(f"{user} > {message}", end='\r')
-
-
-    # Send the message to all clients except the one that sent the messaage
-    for client_socket in CLIENTS:
-        if client_socket != sender_socket:
-                client_socket.send(f"{user} > {message}".encode())
-"""
+irc_instance = IRC_Application()  # The object to handle the IRC side of things.
+default_room = Chatroom(Chatrooms.DEFAULT_ROOM_NAME)    # creates a new default room
+irc_instance.rooms[default_room.name] = default_room  # puts the new default room into the room dictionary
 
 def irc_server():
     #get the host information:
     host = socket.gethostname()
-    port = ChatRooms.PORT_NUMBER #Port defined in chatrooms
+    port = PORT_NUMBER
 
     server_socket = socket.socket()    #the server socket instance
     server_socket.bind((host, port))   #bind the host address and port to the socket on line above
     print(f"Server socket bound to {host}:{port}")
 
     #tell the server how many clients MAX to listen to:
-    server_socket.listen(ChatRooms.MAX_NUMBER_OF_CLIENTS)
+    server_socket.listen(MAX_NUMBER_OF_CLIENTS)
 
     SOCKET_LIST.append(server_socket)
 
-
-    # now a loop to keep doing this forever
-    # TODO: Fix this to exit normally
     while True:
         # Populate a list of sockets that have been read
         read_sockets, write_sockets, err_sockets = select.select(SOCKET_LIST, [], [])
 
         for notified_socket in read_sockets:
-            # Will look for a new connection, then it will create a new instance of a member and put the member
-            # Into a default room
+            # Case where the server socket is being read from (i.e. initial client connection):
+            # Add the client to the socket list and the client dictionary
+            # and add the client to the default room
             if notified_socket == server_socket:
-                """
-                client_socket, client_address = server_socket.accept()
-                SOCKET_LIST.append(client_socket)
-                print(f"New connection established from {client_address}")
+                
+                new_client_socket, new_client_address = server_socket.accept()
+                SOCKET_LIST.append(new_client_socket)
+                print(f"New connection established from {new_client_address}")
 
-                # Initial message data will be username
-                user = client_socket.recv(ChatRooms.MESSAGE_BUFFER).decode()
-                CLIENTS[client_socket] = user
-                client_socket.send(f"Welcome to the server, {user}\n".encode())
-                """
-                new_member_socket, new_client_address = notified_socket.accept()    # gets the connection info
-                new_member_username = irc_Application.getUserName(new_member_socket)    # This get the username from the user entry
-                new_member = Member(new_member_socket, str(new_member_username), ChatRooms.DEFAULT_ROOM_NAME) #creates an instance of a Member
-                irc_Application.rooms[ChatRooms.DEFAULT_ROOM_NAME].Add_New_Member_to_ChatRoom(new_member)   #adds a new user to the chartoom
+                # The initial message data will be the username to add to the client dictionary
+                user = new_client_socket.recv(BUFFER_SIZE).decode()
+                CLIENTS[new_client_socket] = user
+                new_client_socket.send(f"Welcome to the server, {user}\n".encode())
+                
+                irc_instance.rooms[Chatrooms.DEFAULT_ROOM_NAME].add_new_client_to_chatroom(user, new_client_socket)
 
+            # Case where client socket is being read from:
+            # Decode and handle the message
             else:
-                message = notified_socket.recv(ChatRooms.MESSAGE_BUFFER).decode()
+                message = notified_socket.recv(BUFFER_SIZE).decode()
                 
                 # If client disconnected, message will be empty
+                # and client will be removed from socket list and client dictionary
                 if not message:
                     notified_socket.close()
                     SOCKET_LIST.remove(notified_socket)
                     CLIENTS.pop(notified_socket)
+                    # TODO Remove client from all rooms
+                
+                # Send the message to the parser to be handled
                 else:
-                    irc_Application.Message_Parse(notified_socket, message)
+                    IRC_Application.message_parse(irc_instance, notified_socket, CLIENTS[notified_socket], message)
+
 
     server_socket.close()  #gracefully exit
 
